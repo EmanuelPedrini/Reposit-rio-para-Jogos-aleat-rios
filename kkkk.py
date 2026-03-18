2c82d76f6a999c01b7e148e0f6928d32d0841a28
EmanuelPedrini <mistydorydory@gmail.com>
2026-03-18 20:23:31 -0300

    Add files via upload

---
+++ kkkk.py
@@ -0,0 +1,16 @@
+import random
+class Character:
+    def __init__(self, name, acthealth, maxhealth, mana, strength, const)
+        self.name=str("Guerreiro")
+        self.acthealth=maxhealth
+        self.maxhealth=int(10)
+        self.mana=int()
+        self.strength=
+        self.const=
+
+def is_alive(self):
+        return self.acthealth > 0
+
+#Sofrer dano
+def take_damage(self, damage):
+        self.acthealth -= damage
---
+++ retroquestsketch.py
@@ -0,0 +1,7 @@
+import random
+
+iniciar = input("Hallo, do you want to play Retro Quest? \n yes or no")
+if iniciar == yes:
+
+rolld20: = int(random.randint(1,20))
+
